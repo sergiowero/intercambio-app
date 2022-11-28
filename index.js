377cfc69c1f0ffb01d961ea7ffdcb2bdeddb5b94
sergiowero <sergioj.sanchezr@gmail.com>
2022-11-28 10:26:39 -0600

    perimera version con nombres

--- index.js
+++ index.js
@@ -2,5 +2,36 @@
 import './style.css';
 
 // Write Javascript code!
+
+let personotas = [
+  'Panchito',
+  'Majo',
+  'Leo',
+  'checho',
+  'Laura',
+  'Carmen',
+  'Ernesto',
+  'Evan',
+];
+let papelitos = [
+  'Panchito',
+  'Majo',
+  'Leo',
+  'checho',
+  'Laura',
+  'Carmen',
+  'Ernesto',
+  'Evan',
+];
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+while (papelitos.length > 0) {
+  let numero_aleatorio = getRandomInt(papelitos.length);
+  console.log(numero_aleatorio);
+  papelitos.shift();
+}
+
 const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>Intercambio APP</h1>`;
+appDiv.innerHTML = `<h1>Intercambio APP </h1>`;
