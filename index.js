fe3080af2738df5302ea9f8717a4a004c7331e05
sergiowero <sergioj.sanchezr@gmail.com>
2022-12-01 18:51:54 -0600

    2 files modified

--- index.js
+++ index.js
@@ -1,51 +1,29 @@
 // Import stylesheets
 import './style.css';
 
-// Write Javascript code!
+function hacerIntercambio() {
+  let nombres = document.getElementById('nombres').value;
+  let personas = nombres.split(',');
+  let papelitos = nombres.split(',');
+  let asignaciones = {};
 
-let personas = [
-  'Panchito',
-  'Majo',
-  'Leo',
-  'checho',
-  'Laura',
-  'Carmen',
-  'Ernesto',
-  'Evan',
-];
-let papelitos = [
-  'Panchito',
-  'Majo',
-  'Leo',
-  'checho',
-  'Laura',
-  'Carmen',
-  'Ernesto',
-  'Evan',
-];
-let asignaciones = {
-  Panchito: 'Laura',
-};
-
-function getRandomInt(max) {
-  return Math.floor(Math.random() * max);
-}
-
-let iter = 0;
-
-while (papelitos.length > 0) {
-  let numero_aleatorio = getRandomInt(papelitos.length);
-  let nombre = personas[iter];
-  let PapelitoSeleccionada = papelitos[numero_aleatorio];
-
-  if (nombre != PapelitoSeleccionada) {
-    asignaciones[nombre] = PapelitoSeleccionada;
-    papelitos.splice(numero_aleatorio, 1);
-    iter++;
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
   }
+
+  let iter = 0;
+
+  while (papelitos.length > 0) {
+    let numero_aleatorio = getRandomInt(papelitos.length);
+    let nombre = personas[iter];
+    let PapelitoSeleccionada = papelitos[numero_aleatorio];
+
+    if (nombre != PapelitoSeleccionada) {
+      asignaciones[nombre] = PapelitoSeleccionada;
+      papelitos.splice(numero_aleatorio, 1);
+      iter++;
+    }
+  }
+
+  document.getElementById('resultado').innerHTML = JSON.stringify(asignaciones);
 }
-
-console.log(asignaciones);
-
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>Intercambio APP </h1>`;
